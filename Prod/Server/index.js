2a26180aa8465c699aa2911fdbbb33650b63bf94
Jesse-mark <66068892+Jesse-mark@users.noreply.github.com>
2026-04-29 19:19:23 +0530

    changed greeting

--- Prod/Server/index.js
+++ Prod/Server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send({Hi: "Jesse!"})   
+res.send({Hi: "Jesse!", bye: "Mark!"})   
 });
 
 const PORT = process.env.PORT  || 5000;
