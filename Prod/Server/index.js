1ed4a9a39f2e1d0311bf5042506d2ce0b1a21072
Jesse-mark <66068892+Jesse-mark@users.noreply.github.com>
2026-04-30 03:41:19 +0530

    passport - mongodb connection

--- Prod/Server/index.js
+++ Prod/Server/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const passportConfig = require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
+mongoose.connect(keys.mongoURI);
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected')
+})
+
+authRoutes(app);
 
 app.get('/', (req, res) => {
 res.send({Hi: "Jesse!", bye: "Mark!"})   
