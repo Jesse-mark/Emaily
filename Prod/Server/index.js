5c8621a3c0e55676db558a599c5c0afd66e5f887
Jesse-mark <66068892+Jesse-mark@users.noreply.github.com>
2026-04-29 19:11:35 +0530

    fix port log quotes backs

--- Prod/Server/index.js
+++ Prod/Server/index.js
@@ -7,5 +7,5 @@ res.send({Hi: "Jesse!"})
 
 const PORT = process.env.PORT  || 5000;
 app.listen(PORT, ()=> {
-    console.log('server running on port ${PORT}')
+    console.log(`server running on port ${PORT}`)
 });
