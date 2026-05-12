377a98e3e3f822894b1de512a9eb9775e6e42955
Jesse-mark <66068892+Jesse-mark@users.noreply.github.com>
2026-05-12 19:23:25 +0530

    Refactor and MongoDB connect

--- Prod/Server/index.js
+++ Prod/Server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const passportConfig = require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
